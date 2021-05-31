8595d511dc26bf5c290b930fa7466a2fbb0b5d62
Joelyi <37099326+Joelyi@users.noreply.github.com>
2021-05-31 10:16:41 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -6,7 +6,7 @@
 # Blog: https://p3terx.com
 #=================================================
 # Modify default IP
-sed -i 's/192.168.1.1/192.168.5.201/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.2.1/g' package/base-files/files/bin/config_generate
 
 # Modify the version number
 # sed -i 's/OpenWrt/Leopard build $(date "+%Y.%m.%d") @ OpenWrt/g' package/default-settings/files/zzz-default-settings
@@ -16,4 +16,3 @@ sed -i 's/192.168.1.1/192.168.5.201/g' package/base-files/files/bin/config_gener
 
 # 去除默认主题
 sed -i '/set luci.main.mediaurlbase=\/luci-static\/bootstrap/d' feeds/luci/themes/luci-theme-bootstrap/root/etc/uci-defaults/30_luci-theme-bootstrap
-
